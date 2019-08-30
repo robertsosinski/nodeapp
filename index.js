b92b5f25c6d4dca3727d08dd5428b085c9f5186f
Robert Sosinski <robertsosinski@users.noreply.github.com>
2019-08-30 10:21:30 -0500

    update

--- index.js
+++ index.js
@@ -2,27 +2,30 @@ const http = require("http");
 
 const port = 8080;
 
-const {Client} = require("pg");
+const {Pool} = require("pg");
 
-const client = new Client({
+const pool = new Pool({
   host: process.env.APP_DBHOST,
   user: process.env.APP_DBUSER,
   port: process.env.APP_DBPORT,
   database: process.env.APP_DBNAME,
   password: process.env.APP_DBPASS
 });
-client.connect();
 
-const server = http.createServer((req, res) => {
+pool.connect();
+
+const server = http.createServer(async (req, res) => {
   console.log(`${new Date()}: ${req.method}: ${req.url}`);
 
-  client.query("SELECT version()::text as version", [], (err, dbres) => {
-    console.log(err ? err.stack : dbres.rows[0].version);
+  let dbres = await pool.query("SELECT version()::text as version, now()::text as timestamp", []);
+  let version = dbres.rows[0].version;
+  let timestamp = dbres.rows[0].timestamp;
+  
+  console.log(timestamp, version);
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(`REQ_URL: ${req.url}\nNODE_ENV: ${process.env.NODE_ENV}\nAPP_DBHOST: ${process.env.APP_DBHOST}\nDB_VERSION: ${dbres.rows[0].version}`);
-  });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end(`REQ_URL: ${req.url}\nNODE_ENV: ${process.env.NODE_ENV}\nAPP_DBHOST: ${process.env.APP_DBHOST}\nDB_VERSION: ${version}\nDB_TIMESTAMP: ${timestamp}`);
 });
 
 server.listen(port);
